72cc48189ba202e4a9f6364ea4ff2fe97ed7614e
João Pedro <joaogood@outlook.com>
2024-09-08 20:33:28 -0300

    att deploy

--- Flask/config.py
+++ Flask/config.py
@@ -8,5 +8,4 @@ SQLALCHEMY_TRACK_MODIFICATIONS = False
 CORS_HEADERS = 'Content-Type'
 SECRET_KEY = os.getenv('SECRET_KEY')
 JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY')
-
-                                                
+FLASK_DEBUG = True
